5e5daf45978c7e702f0cbaebc643935cba7a1db0
Rahul Anand <RRSIMT.RAHUL@GMAIL.COM>
2023-06-17 14:33:21 +0400

    Expenditure analysis code

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,11 +5,31 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 
   Once you've implemented the logic, test your code by running
-  - `npm run test-expenditure-analysis`
+  - `npm run test-expenditur1qe-analysis`
 */
-
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function calculateTotalSpentByCategory(ts) {
+  var aryCatgPrice =[];
+  for (let i = 0; i < ts.length; i++) 
+  {
+    var curCatg = ts[i].category;
+    var curPrice = ts[i].price;
+    var item = GetCategoryItem(aryCatgPrice,curCatg);
+    var index = aryCatgPrice?aryCatgPrice.length:0;
+    if(item){
+      item.totalSpent+=ts[i].price;
+    }
+    else
+    {
+      aryCatgPrice[index]= {'category':curCatg, 'totalSpent':curPrice};
+    }    
+  }
+  return aryCatgPrice;
 }
-
+function GetCategoryItem(aryCatgPrice, catg){
+  for (let i = 0; i < aryCatgPrice.length; i++) {
+    if(aryCatgPrice[i].category.includes(catg)){
+      return aryCatgPrice[i];
+    }
+  }
+}
 module.exports = calculateTotalSpentByCategory;
